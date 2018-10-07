5df004c1f7dcf6126a3d94a6ca0e6d572c7bc25f
xjtuyanshi <xjtuyanshi@gmail.com>
2018-10-07 01:02:18 -0700

    Add camera_cal and undistort/transform code

---
+++ camera_cal.py
@@ -0,0 +1,56 @@
+import numpy as np
+import cv2
+import glob
+import matplotlib.pyplot as plt
+import pickle
+# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+objp = np.zeros((6*9,3), np.float32)
+objp[:,:2] = np.mgrid[0:9,0:6].T.reshape(-1,2)
+
+# Arrays to store object points and image points from all the images.
+objpoints = [] # 3d points in real world space
+imgpoints = [] # 2d points in image plane.
+
+# Make a list of calibration images
+images = glob.glob('camera_cal/calibration*.jpg')
+
+# Step through the list and search for chessboard corners
+for fname in images:
+    img = cv2.imread(fname)
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+    # Find the chessboard corners
+    ret, corners = cv2.findChessboardCorners(gray, (9,6),None)
+
+    # If found, add object points, image points
+    if ret == True:
+        objpoints.append(objp)
+        imgpoints.append(corners)
+
+        # Draw and display the corners
+        img = cv2.drawChessboardCorners(img, (9,6), corners, ret)
+
+        plt.imshow(img)
+        plt.plot()
+        cv2.waitKey(1500)
+
+#cv2.destroyAllWindows()
+ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+calibrated_camera_info ={"mtx":mtx,"dist":dist}
+pickle.dump(calibrated_camera_info,open("calibrated_camera_info.p","wb"))
+
+# test undistorted
+
+# for fname in images:
+#     img = cv2.imread(fname)
+#     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#     undistorted = cv2.undistort(img, mtx, dist, None, mtx)
+#     f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
+#     f.tight_layout()
+#     ax1.imshow(img)
+#     ax1.set_title('Original Image', fontsize=50)
+#     ax2.imshow(undistorted)
+#     ax2.set_title('Undistorted Image', fontsize=50)
+#     plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
+#     plt.show()
+
